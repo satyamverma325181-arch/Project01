580288569f00472f06a57623cfb620cee1e8a254
satyamverma325181-arch <satyamverma325181@gmail.com>
2026-09-21 13:24:32 +0530

    update App

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
   
 const Cards = () => {
+  const age=21
   return (
-    <div>Cards</div>
+    <div className='card'>
+      <h1>hlo everyone , i am kartik </h1>
+      <h2>and i am {age} year old </h2>
+    </div>
   )
 }
 
