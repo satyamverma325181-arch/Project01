fe553ef0f9b7c0f4dc1b8caa529ca2fa696bb0d1
satyamverma325181-arch <satyamverma325181@gmail.com>
2026-09-23 21:05:25 +0530

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import Cards from './components/Cards.jsx'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { CiBookmark } from "react-icons/ci";
+import Cards from './components/Cards';
 
 
 const App = () => {
-const user="satyam"
   return (
-    <div>
-      <Navbar/>
-      <h1>hlo everyone , i am {user} </h1>
+   <div className='parents'>
+     <Cards/>
       <Cards/>
-      <Footer/>
-    </div>
+   </div>
     
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import { CiBookmark } from "react-icons/ci";
 import './Cards.css'
-  
+
 const Cards = () => {
-  const age=21
   return (
-    <div className='cards'>
-      <h1>hlo everyone , i am kartik </h1>
-      <h2>and i am {age} year old </h2>
-    </div>
+  <div className='parents'>
+      <div className="card">
+        <div className="top">
+          <img src="https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg" alt=""  cl/>
+          <button> save <CiBookmark /></button>
+        </div>
+  
+        <div className="center">
+            <h3>amazon <span>5 days ago</span></h3>
+            <h2>senior ui/ux designer</h2>
+            <div className='tag'><h4>part time</h4>
+            <h4>senior level</h4>
+            </div>
+        </div>
+  
+        <div className="bottom">
+      <div>
+        
+          <h3>$120/hours</h3>
+          <p>mumbai, pune</p>
+        
+      </div>
+      <button>apply now</button>
+        </div>
+      </div>
+  
+
+  
+     
+      </div>
   )
 }
 
